68578358d2ff688a1c340d360abce09b2a0b9d15
Vinicius Gabriel <88984052+VManock@users.noreply.github.com>
2022-10-20 17:33:19 -0300

    Update script.js

--- main/script.js
+++ main/script.js
@@ -2,16 +2,13 @@ async function pegaDados () {
     try {
         var consultaDados = await fetch('https://api.themoviedb.org/3/trending/all/day?api_key=1eb455a4d8c509c76af086bb28348f6a');
         var dadosConvertidos = await consultaDados.json();
-        if (dadosConvertidos.erro) {
-            throw Error('Filme não existente!');
-        }
-
+    
         var poster = document.getElementById('poster');
         var titulo = document.getElementById('titulo');
         var sinopse = document.getElementById('sinopse');
         var nota = document.getElementById('nota');
         
-        poster.value = dadosConvertidos.find((title));
+        poster.innerHTML = dadosConvertidos.find((title));
         titulo.value = dadosConvertidos[0].title;
         sinopse.value = dadosConvertidos[0].overview;
         nota.value = dadosConvertidos[0].vote_average;
@@ -19,7 +16,7 @@ async function pegaDados () {
         console.log(dadosConvertidos);
         console.log(poster, titulo, sinopse, nota);
 
-        return21;
+        return;
     } catch (erro) {
         console.log(erro)
     }
@@ -29,7 +26,35 @@ async function pegaDados () {
 pegaDados();
 
 
+/* async function pegaDados () {
 
+    var container = document.getElementById('container')
+    var consultaDados = await fetch('https://api.themoviedb.org/3/trending/all/day?api_key=1eb455a4d8c509c76af086bb28348f6a');
+    var dadosConvertidos = await consultaDados.json();
+
+    (dadosConvertidos) => {
+        dadosConvertidos.results.forEach((movie)) => {
+            console.log(movie)
+            if(movie.media_type !== 'tv') {
+
+                let image = "https://image.tmdb.org/t/p/original/" + movie.poster_path;
+
+                let card = `
+                    ${movie.title}
+                    ${movie.overview}
+                    ${image}
+                    ${movie.average}
+
+                ` //html code
+                
+                container.innerHTML += card
+            }
+        }
+
+
+}}
+
+*/
 
 
 
